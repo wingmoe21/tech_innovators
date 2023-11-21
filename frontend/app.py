05422d777f778119221db54d99b08ebf5c7476ec
mald2 <maldossary16@gmail.com>
2023-11-21 19:56:18 +0300

    fixed buttons on the right side

--- frontend/app.py
+++ frontend/app.py
@@ -9,5 +9,14 @@ def home():
 def chatbot():
     return render_template('page1.html')  # Replace with your home page file name
 
+@app.route('/summary')
+def summary():
+    return render_template('summary.html')  # Replace with your home page file name
+
+@app.route('/quiz')
+def quiz():
+    return render_template('quiz.html')  # Replace with your home page file name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
