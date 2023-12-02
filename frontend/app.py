80f8e0b33b7807fd3013af39c973b6e73b491332
mald2 <maldossary16@gmail.com>
2023-12-02 21:49:26 +0300

    finalized frontend

--- frontend/app.py
+++ frontend/app.py
@@ -17,7 +17,7 @@ Session(app)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/chatbot', methods=['GET', 'POST'])
 def chat():
